565160c014f7d09e198c2ade5f02e42e9b0e1bec
Udaykanth R <udaykanth.rapeti@gmail.com>
2016-07-23 20:07:17 +0530

    fixed formating issues. and added HEX format view.

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void encipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
 void decipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
   unsigned int i;
   uint32_t v0=v[0], v1=v[1], delta=0x9E3779B9, sum=delta*num_rounds;
-  for (i=0; i < num_rounds; i++) {
+  while(sum) {
     v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
     sum -= delta;
     v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
@@ -125,7 +125,17 @@ void doBlock(uint8_t *d, uint8_t *key, int isEncrypt) {
 
   }
 
-  printf("encrypted/decrepted string is: %s\n", buffer);  
+  printf("encrypted/decrepted string is:\n");
+  printf("HEX:\n");
+  for(i=0;i<32;i++) {
+    printf("%0x ", buffer[i]);
+  }
+  printf("\n");
+  printf("Chars:\n");
+  for(i=0;i<32;i++) {
+    printf("%c", buffer[i]);
+  }
+  printf("\n");
 }
 
 
@@ -139,13 +149,20 @@ void decrypt(uint8_t *d, uint8_t *key) {
 int main() {
 
   uint8_t *d = "This is a sample encryption TEXT";
-  uint8_t *key = "mmmmmmmmmmmmmmmm";
+  //  
+  /* uint8_t d[] = {af1b916ca51b3c46b7bc5d397c71d7317656933577a222e49e5e4f28637a67239252df062b4a43a7}; */
+  /* uint8_t d[] = {0xaf, 0x1b, 0x91, 0x6c, 0xa5, 0x1b, 0x3c, 0x46, 0xb7, 0xbc, 0x5d, 0x39, 0x7c, 0x71, 0xd7, 0x31, 0x76, 0x56, 0x93, 0x35, 0x77, 0xa2, 0x22, 0xe4, 0x9e, 0x5e, 0x4f, 0x28, 0x63, 0x7a, 0x67, 0x23, 0x92, 0x52, 0xdf, 0x06, 0x2b, 0x4a, 0x43, 0xa7,}; */
+  
+  /* uint8_t *key = "mmmmmmmmmmmmmmmm"; */
+  // ed 10 b7 40 1c d3 4f c1 b2 26 97 d7 f6 16 47 a2
+  // 54 49 31 38 32 34 33 35 36 37 33 34 39 31 53 5f
+  uint8_t key[] = {0x54, 0x49, 0x31, 0x38, 0x32, 0x34, 0x33, 0x35, 0x36, 0x37, 0x33, 0x34, 0x39, 0x31, 0x53, 0x5f,};
   p=0;
   encrypt(d, key);
   uint8_t e_data[100];
   int i=0;
-  int len = strlen(d);
-  for (i=0;i<len;i++) {
+  /* int len = strlen(d); */
+  for (i=0;i<32;i++) {
     e_data[i] = buffer[i];
   }
   p=0;
